f35a504249ac7ee64d16263d05206ff47cd9230f
yaoguangluo <fengyue1985>
2019-01-26 22:44:56 +0800

    Did 300 rows today, need leave now. thanks, will spend more time for thinking about the devops workflow.
    yaoguang

---
+++ src/org/vpcs/pillows/sets/DemensionSet.java
@@ -0,0 +1,10 @@
+package org.vpcs.pillows.sets;
+
+import javax.swing.ImageIcon;
+
+//this file is use for the app engine configuration.
+public class DemensionSet {
+	static int ApplicationHeigh;
+	static int ApplicationWidth;
+	static ImageIcon logo;
+}
