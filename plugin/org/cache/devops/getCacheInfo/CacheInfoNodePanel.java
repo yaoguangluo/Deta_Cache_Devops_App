7df096ddf4aefb885e34cadf0aa49b2a5c935b11
yaoguangluo <fengyue1985>
2019-02-14 01:35:10 +0800

    socket 请求服务器获取数据

--- plugin/org/cache/devops/getCacheInfo/CacheInfoNodePanel.java
+++ plugin/org/cache/devops/getCacheInfo/CacheInfoNodePanel.java
@@ -7,6 +7,11 @@ import org.LYG.GUI.OSGI.*;
 
 import java.awt.Panel;
 import java.awt.ScrollPane;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.awt.Color;
 public class CacheInfoNodePanel extends objectPanel{
 	private static final long serialVersionUID = 1L;
@@ -23,12 +28,12 @@ public class CacheInfoNodePanel extends objectPanel{
 		label.setBounds(2, 2, 80, 30);
 		JTextField ipport= new JTextField("xxx.xxx.xxx.xxx:xxxxx", 18);
 		ipport.setBounds(2+85, 2, 200, 30);
-		
+
 		JLabel labelName= new JLabel("登录名称：");
 		labelName.setBounds(2, 2+35, 80, 30);
 		JTextField jTextFieldName= new JTextField("输入用户名称", 18);
 		jTextFieldName.setBounds(2+85, 2+35, 200, 30);
-		
+
 		JLabel labelPswd= new JLabel("登录密码：");
 		labelPswd.setBounds(2, 2+35+35, 80, 30);
 		JTextField jTextFieldPswd= new JTextField("输入用户密码", 18);
@@ -36,12 +41,33 @@ public class CacheInfoNodePanel extends objectPanel{
 		//
 		JButton jbutton=new JButton("确定");
 		jbutton.setBounds(2, 2+35+35+35, 80, 30);
+		jbutton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					String textOfLabelName = labelName.getText();
+					String textOflabelPswd = labelPswd.getText();
+					String textOfIpport = ipport.getText();
+					Socket socket = new Socket(textOfIpport.split(":")[0],Integer.valueOf(textOfIpport.split(":")[1]));
+					PrintWriter os = new PrintWriter(socket.getOutputStream());
+					//os.println("/111");
+					//添加输入值
+					os.flush();
+					InputStream is = socket.getInputStream();
+					byte[] bytes=new byte[1024];
+					while((is.read(bytes, 0, 1024)) != -1) {
+						//System.out.println("1"+new String(bytes)); 
+						//得到输出值
+					}			
+				}catch(Exception e) {
+
+				}
+			}});
 		jp.add(labelName);
 		jp.add(jTextFieldName);
 		jp.add(labelPswd);
 		jp.add(jTextFieldPswd);
-		
-		
+
+
 		jp.add(label);
 		jp.add(ipport);
 		jp.add(jbutton);
